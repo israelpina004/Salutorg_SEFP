3d8ba216fd2280dc871f492856aa6f00478abd31
epadilla03 <emmanuelle.padilla03@gmail.com>
2024-11-11 22:23:46 -0500

    Added Header and Dropdown

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
 import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
-import Home from "./Pages/Home"
-import Header from "./Components/Header";
-import MainLayout from "./Components/MainLayout";
+import Home from "./Pages/Home/home";
+import Header from "./Components/Header/header";
 
 function App() {
   return (
     <>
     <BrowserRouter>
-      <MainLayout />
       <Header />
       <Routes>
         <Route path = "/" exact={true} element={<Home/>} />
