a82addca9ae2347d2e6a64e2f821d140fba3eeb2
Akram Uddin <uddinakram1113@gmail.com>
2024-12-01 18:40:11 -0500

    Added balance component using static user id for now

--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -1,11 +1,13 @@
 import Header from "../../Components/Header/header"
 import MainContent from "../../Components/Main/MainContent";
+import Balance from "../../Components/Balance/Balance";
 
 const Home =()=> {
     return (
         <>
             <Header />
             <MainContent />
+            <Balance/>
         </>
     )
 }
