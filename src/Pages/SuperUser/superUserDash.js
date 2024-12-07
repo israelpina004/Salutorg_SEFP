42521dd8dbcd8bceb784d7f91eecbbec9b29f288
Desiree Caceres <116980242+desi-C@users.noreply.github.com>
2024-12-06 20:56:29 -0500

    Update superUserDash.js

--- src/Pages/SuperUser/superUserDash.js
+++ src/Pages/SuperUser/superUserDash.js
@@ -1,16 +1,47 @@
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
 import "./superUserDash.css";
 
 function SuperUserDashboard() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isActive, setIsActive] = useState('approvals');
+
+  const handleSignOut = () => {
+    setIsLoggedIn(false);
+    alert("You've signed out. See you soon!")
+  };
+
+  if (!isLoggedIn) {
+    return <Navigate to="/login" replace />;
+  }
+
+  const handleClick = (tab) => {
+    setIsActive(tab);
+  }
+
     return (
       <div>
-        <h1>SuperUser Dashboard</h1>
-        <nav>
-          <ul>
-            <li><Link to="/admin/approvals">Account Approvals</Link></li>
-            <li><Link to="/admin/suspensions">Suspension Appeals</Link></li>
-          </ul>
-        </nav>
+        <div className="admin-dash">
+          <aside className="sidebar">
+            <h1>Navigation</h1>
+              <ul>
+                <li onClick={() => handleClick('approvals')}
+                style={{ backgroundColor: isActive == 'approvals' ? '#cc3f07' : 'transparent' }}>
+                  Account Approvals</li>
+                  <li onClick={() => handleClick('suspensions')}
+                style={{ backgroundColor: isActive == 'suspensions' ? '#cc3f07' : 'transparent' }}>
+                  Suspension Appeals</li>
+                <li onClick={handleSignOut}>Sign Out</li>
+              </ul>
+          </aside>
+
+          <main className="main">
+            <h1>SuperUser Dashboard</h1>
+            <p1>Welcome back! Use Navigation to check pending account approvals, or handle any suspensions.</p1>
+              {((isActive == 'approvals') && <p>Test</p>) || 
+              ((isActive == 'suspensions') && <p>Test but like,,, the other one</p>)}
+          </main>
+        </div>
       </div>
     );
   }
