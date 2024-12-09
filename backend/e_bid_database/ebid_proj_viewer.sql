98cabe50f7fb4fa89cced36297370251c934918d
Desiree Caceres <116980242+desi-C@users.noreply.github.com>
2024-12-08 21:21:28 -0500

    Update ebid_proj_viewer.sql

--- backend/e_bid_database/ebid_proj_viewer.sql
+++ backend/e_bid_database/ebid_proj_viewer.sql
@@ -5,7 +5,5 @@ DROP TABLE IF EXISTS `viewer`;
 
 CREATE TABLE `viewer` (
   `viewer_ID` int NOT NULL AUTO_INCREMENT,
-  `view_history` varchar(45) DEFAULT NULL,
-  `session_ID` int NOT NULL,
   PRIMARY KEY (`viewer_ID`)
 );
