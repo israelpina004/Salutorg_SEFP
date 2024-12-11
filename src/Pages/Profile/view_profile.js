fd1fa5f0d69ae832dd05889e94de65b03548477c
Desiree Caceres <116980242+desi-C@users.noreply.github.com>
2024-12-11 16:21:03 -0500

    Update view_profile.js

--- src/Pages/Profile/view_profile.js
+++ src/Pages/Profile/view_profile.js
@@ -11,32 +11,7 @@ const Profile = () => {
                         <img width = {120} src = { Logo2 } alt="" />
                     </Link>
                 </div>
-
-                {/* Tabs Navigation */}
-                <div className="tabs-container w-100 p-3 border-bottom border-gray-300">
-                    <ul className="nav nav-tabs justify-content-center">
-                        <li className="nav-item">
-                            <Link className="nav-link active" to="/account">
-                                Account
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/security">
-                                Security
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/addresses">
-                                Addresses
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/payment-details">
-                                Payment Details
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
+                <p align="center"> <i>Click the logo above to return to the hompage!</i> </p>
 
                 {/* About Section */}
                 <div className="container p-4">
@@ -44,30 +19,10 @@ const Profile = () => {
                         {/* About You */}
                         <div className="col-md-6 mb-4">
                             <div className="card shadow-sm p-4">
-                                <h5>About You</h5>
-                                <p>
-                                    <strong>Name:</strong> User Name
-                                </p>
-                                <p>
-                                    <strong>Member Since:</strong> Date Here 
-                                </p>
-                                <button className="btn btn-outline-dark">
-                                    Edit public profile
-                                </button>
-                            </div>
-                        </div>
-
-                        {/* Location Settings */}
-                        <div className="col-md-6 mb-4">
-                            <div className="card shadow-sm p-4">
-                                <h5>Location Settings</h5>
-                                <p>Set where you live, what language you speak, and the currency you use.</p>
-                                <label htmlFor="region">Region:</label>
-                                <select id="region" className="form-control mt-2">
-                                    <option value="us">United States</option>
-                                    <option value="ca">Canada</option>
-                                    <option value="uk">United Kingdom</option>
-                                </select>
+                                <h5 align="center">About You</h5>
+                                <p> <b>Name:</b> User Name  </p>
+                                <p> <b>Email:</b>   </p>
+                                <p> <b>Rating:</b>   </p>
                             </div>
                         </div>
                     </div>
